9612c9c372b54a4ed17d3b51d853dafe055ff070
Thomas Vladeck <thomas.vladeck@gmail.com>
2020-02-20 18:50:50 -0500

    the model is returning and unconstrained solution
    i wanted to put through an example that i worked through from scratch,
    but it appears there is something wrong
    
    i included a very simple example to find the least cost solution to
    (-3 - xhat1)^2 + (3 - xhat2)^2 such that xhat1, xhat2 >=0
    
    the routine is returning (-3, 3) which violates the inequality
    constraint

--- quadprog/quadprog_model_compile_test.R
+++ quadprog/quadprog_model_compile_test.R
@@ -9,21 +9,8 @@ model <- stan_model(file = "quadprog_test_model.stan",
                verbose = FALSE)
 print(model@model_name)
 
-# Quadprog formulation
-library(quadprog)
-## Assume we want to minimize: -(0 5 0) %*% b + 1/2 b^T b
-## under the constraints: A^T b >= b0
-## with b0 = (-8,2,0)^T
-## and       (-4 2 0)
-## A = (-3 1 -2)
-##     ( 0 0 1)
-## we can use solve.QP as follows:
-Dmat <- matrix(0,3,3)
-diag(Dmat) <- 1
-dvec <- c(0,5,0)
-Amat <- matrix(c(-4,-3,0,2,1,0,0,-2,1),3,3)
-bvec <- c(-8,2,0)
-solve.QP(Dmat,dvec,Amat,bvec=bvec)
+
+
 
 # stan formulation
 # min 0.5 * x G x + g0 x
@@ -32,7 +19,7 @@ solve.QP(Dmat,dvec,Amat,bvec=bvec)
 # CI^T x + ci0 >= 0
 # 
 # The matrix and vectors dimensions are as follows:
-#   G: n * n
+# G: n * n
 # g0: n
 # 
 # CE: n * p
@@ -42,40 +29,77 @@ solve.QP(Dmat,dvec,Amat,bvec=bvec)
 # ci0: m
 # 
 # x: n
-G = Dmat
-g0 = dvec
-CI = Amat
-ci0 = -bvec
-CE = matrix(0, 3, 3)
-ce0 = c(0,0,0)
-x = bvec
 
-# dimensionality checks
-0.5 * x %*% G %*% x + g0 %*% x
-CE %*% x + ce0
-CI %*% x + ci0
+# a simple example to make things easier
+# say we have x1 = -3 and x3 = 3
+# we want to find xhat s.t. we minimize (x-xhat)^2
+# while constraining xhat >= 0
+
+#    (x1 - xhat1)^2 + (x2 - xhat2)^2
+# => (-3 - xhat1)^2 + ( 3 - xhat2)^2
+# => 9 + 6xhat1 + xhat1^2 + 9 - 6xhat2 + xhat2^2
+# removing constants
+# => xhat1^2 + xhat2^2 + 6xhat1 - 6xhat2
+
+confirmfn = function(par) {
+  xhat1 = par[1]
+  xhat2 = par[2]
+  xhat1^2 + xhat2^2 + 6*xhat1 - 6*xhat2
+}
+
+optim(c(0,0), confirmfn)$par
+
+# => G  = [2   0]
+#         [0   2]
+#    g0 = [6  -6]
+
+# because 
+#                     [2   0] [xhat1]          [xhat1]
+# 0.5 * [xhat1 xhat2] [0   2] [xhat2] + [6 -6] [xhat2]
+# is equal to 
+# xhat1^2 + xhat2^2 + 6xhat1 - 6xhat2
+
+confirmfn2 = function(par) {
+  xhat1 = par[1]
+  xhat2 = par[2]
+  G = diag(2, 2, 2)
+  g0 = c(6, -6)
+  x = matrix(c(xhat1, xhat2), 1, 2)
+  
+  0.5 * x %*% G %*% t(x) + g0 %*% t(x)
+}
+
+optim(c(0,0), confirmfn2)$par
+
+G = diag(1, 2, 2) * 2
+g0 = c(6, -6)
+xstart = c(-3, 3) 
+CE = matrix(0, 2, 2)
+ce0 = rep(0, 2)
+CI = diag(1, 2, 2)
+ci0 = rep(0, 2)
 
 stan_data = list(
-  N = 3,
+  N = 2,
+  P = 2,
+  M = 2,
   G = G,
   g0 = g0,
   CI = CI,
   ci0 = ci0,
   CE = CE,
   ce0 = ce0,
-  x = x
+  x = xstart
 )
 
 idk = sampling(
   model,
   stan_data,
-  iter = 1
+  iter = 1,
+  chains = 1
 )
 
 idfk = extract(idk)
+xhat = idfk$x[1, ]
 
-
-
-
-
-
+all(t(CI) %*% xhat + ci0 >= rep(0, 2))
